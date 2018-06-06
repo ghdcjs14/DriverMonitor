55ee42b8e674201d3d2360bdea30a5bc8b85fba0
hong <black1990-14@hanmail.net>
2018-06-06 13:59:10 +0900

    return face detect results

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -98,67 +98,62 @@ extern "C" {
                          real_facesize_y + real_facesize_height / 2);
             rectangle(img_result, Point(real_facesize_x, real_facesize_y)
                     , Point(real_facesize_x + real_facesize_width, real_facesize_y + real_facesize_height), Scalar(255, 0, 0), 30, 8, 0);
-//            ellipse(img_result, center, Size(real_facesize_width / 2, real_facesize_height / 2), 0,
-//                    0, 360,
-//                    Scalar(255, 0, 255), 30, 8, 0);
 
 
             Rect face_area(real_facesize_x, real_facesize_y, real_facesize_width,
                            real_facesize_height);
             faceROI = img_input(face_area);
-            split(img_input, channels); // 0: Blue, 1: Green, 2: Red
 
-            // 1. faceROI의 색깔 별 평균 구하기
+            // 1. faceROI를 RGB channels로 split한다.
+            split(faceROI, channels); // 0: Blue, 1: Green, 2: Red
+
+            // faceROI의 색깔 별 평균 구하기
             //meanR = mean(channels[2]);
-//            meanG = mean(channels[1]);
+            //meanG = mean(channels[1]);
             //meanB = mean(channels[0]);
+            //greenTraceQueue.push(meanG.val[0]);
 
-//            greenTraceQueue.push(meanG.val[0]);
-
-
-
+            // 2. green 픽셀들을 normalize 한다.
             normalize(channels[1], normGreen,0,1,NORM_MINMAX,CV_32F);
 
+            // 3. normalize 된 green 픽셀들에 대해 DFT를 통해 주파수 영역으로 분리 시킨다.
             dft(normGreen, dftGreen, DFT_COMPLEX_OUTPUT);
 
-//            double magGreen = max(magnitude(dftGreen.d,dftGreen[1],dftGreen[0]))
-
-
-
+            // dft의 평균
             double meanval0 = mean(dftGreen).val[0];
-//            double meanval1 = mean(dftGreen).val[1];
-//            double meanval2 = mean(dftGreen).val[2];
-//            double meanval3 = mean(dftGreen).val[3];
-//            __android_log_print(ANDROID_LOG_DEBUG, "native-lib :: ",
-//                                "meanval0: %.2f", meanval0);
-//            __android_log_print(ANDROID_LOG_DEBUG, "native-lib :: ",
-//                                "meanval1: %.2f", meanval1);
-//            __android_log_print(ANDROID_LOG_DEBUG, "native-lib :: ",
-//                                "meanval2: %.2f", meanval2);
-//            __android_log_print(ANDROID_LOG_DEBUG, "native-lib :: ",
-//                                "meanval3: %.2f", meanval3);
+
+            // 4. 주파수 영역의 x축 y축을 이용하여 magnitude를 구하고 max 값을 추출한다.
             Mat magGreen, gx, gy;
+            double minVal,maxVal;
+
             Sobel(dftGreen, gx, dftGreen.depth(), 1,0,3);
             Sobel(dftGreen, gy, dftGreen.depth(), 0,1,3);
             magnitude(gx,gy,magGreen);
-            double minVal,maxVal;
-            minMaxLoc(magGreen, &minVal, &maxVal);
-//            double fps = 15;
-            double tempFeq = maxVal / (dftGreen.rows * dftGreen.cols);
-            double bpm = tempFeq * 60;
+            minMaxLoc(abs(magGreen), &minVal, &maxVal);
 
+            // 5. Max magnitude를 이용하여 주파수를 얻고 bpm으로 바꾼다.
+            double fps = 5;
+            double feq = 0; // 초기 주파수 값
+            double tempFeq = maxVal * fps / (dftGreen.rows * dftGreen.cols);
 
-//            double tempFeq = magGreen.val[0] * 60.0 / (double)(dftGreen.rows * dftGreen.cols) ;
+            if(tempFeq >= 0.75 && tempFeq >= 4) {
+                // 초기 주파수가 측정 안되어 있을 때, 이전 bpm과 차이가 12bpm 이상 나는 것은 무시
+                if(feq == 0 || abs(feq-tempFeq)*60 <= 12) {
+                    feq = tempFeq;
+                }
+            }
+            double bpm = feq * 60;
 
+            // 6. bpm을 화면에 출력한다.
             meanval0 = meanval0 * 60;
             char ss[256];
-            sprintf(ss,"%.2f",meanval0);
+            sprintf(ss,"%.2f",tempFeq);
             putText(img_result, ss
                     , Point(real_facesize_x + real_facesize_width + 10, real_facesize_y)
                     , FONT_HERSHEY_SIMPLEX
                     , 2, Scalar(255,255,255), 2);
 
-            sprintf(ss,"%.2f",bpm);
+            sprintf(ss,"%.2f",feq);
             putText(img_result, ss
                     , Point(real_facesize_x + real_facesize_width + 10, real_facesize_y+50)
                     , FONT_HERSHEY_SIMPLEX
